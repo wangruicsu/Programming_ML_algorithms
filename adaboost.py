aefec0535dca52480158c08ed37ccc510be30953
wangrui <wangruicsu@gmail.com>
2018-11-20 00:21:44 +0800

    get_Gmx()  if/else

--- adaboost.py
+++ adaboost.py
@@ -36,14 +36,36 @@ def loadData():
     return x, y #返回数据特征和数据类别
 
 # 基学习器
-def Gmx(x,y):
+def get_Gmx(x,y,D,fx):
+#    print(D)
     Gmx_all = []
+    Gmxi_not_yi = []
     em = []
+    pred = []
+#    item = 0
+    pred = []
+
     for i in range(len(x) - 1):
+        item = 0
         Gmx_all.append((x[i] + x[i+1])/2.0)
-        pred = [1]*int(math.ceil(Gmx_all[i])) + [-1]*int(len(x) - math.ceil(Gmx_all[i]))
-        em.append(1 - list(map(lambda x: x[0]-x[1], zip(y, pred))).count(0)/len(x))
+        if fx:
+#            pred = [1]*int(math.ceil(Gmx_all[i])) + [-1]*int(len(x) - math.ceil(Gmx_all[i]))
+            for j in range(len(fx)):
+                am = fx[j]
+                item = [1]*int(math.ceil(am)) + [-1]*int(len(x) - math.ceil(am))
+                pred_am = [am*k for k in item]
+                pred = list(map(lambda x: x[0]+x[1], zip(pred, pred_am)))
+        else:
+            pred = [1]*int(math.ceil(Gmx_all[i])) + [-1]*int(len(x) - math.ceil(Gmx_all[i]))
+        Gmxi_not_yi = list(map(lambda x: x[0]-x[1], zip(y, pred)))
+#        print(Gmxi_not_yi)
+        item1 = 0
+        for n in range(len(D)):
+            if Gmxi_not_yi[n] != 0: 
+                item1 += D[n]
+        em.append(item1)
     # 基分类器
+    print(em)
     Gmx = Gmx_all[em.index(min(em))]
     # 计算基学习器系数 am
     am = 1/2 * math.log((1-min(em))/min(em))
@@ -58,25 +80,46 @@ def calW(D_old,am,y,y_Gmx):
     for i in range(len(y)):
         Zm_item.append(D_old[i]*math.exp(-am*y[i]*y_Gmx[i]))
     Zm = sum(Zm_item)
-    D_new = list(map(lambda x: x[0]*x[1]/Zm, zip(D_old, Zm_item)))
+    D_new = [i/Zm for i in Zm_item]
+#    D_new = list(map(lambda x: x[0]*x[1]/Zm, zip(D_old, Zm_item)))
     return D_new
     
-# 参数：x_list,y_list,m:基学习器数量
-def AdaBoost(x,y,m):
-    am, Gmx,y_Gmx = Gmx(x,y)
-    D1 = [1/len(x)]*len(x)
-    D2 = calW(D1,am,y,y_Gmx)
-    fx = 0
-    for i in range(m):
-        pass
-       
-    Gx = np.sign(fx)
-    return Gx
+# 参数：x_list,y_list,maxIterNum:基学习器数量,errorThreshold:分类器误差阈值
+def AdaBoost(x,y,maxIterNum,errorThreshold):
+    D1 = [1.0/len(x)]*len(x)
+    fx_err = 0
+    D = D1
+    amGmx = [] #am * Gmx 后的结果存储
+    Gmx_all = []  #
+    am = 0
+    am_all = []
+    Gmx = 0
+    Gx = [0]*len(x)
+    fx = []
+    for i in range(maxIterNum):
+#        print(D)
+        am, Gmx,y_Gmx = get_Gmx(x,y,D,fx)
+        fx.append(am)
+        
+        Gmx_all.append(['am:',am,'Gmx: ',Gmx])
+        
+        amGmx = [am*j for j in y_Gmx]
+        #更新的分类器 的 分类误差
+        Gx = list(map(lambda x: x[0]+x[1], zip(Gx, amGmx)))
+        Gx_sign = np.sign(Gx)
+        fx_err = 1 - list(map(lambda x: x[0]-x[1], zip(y, Gx_sign))).count(0)/len(x)
+        print(fx_err)
+        if fx_err < errorThreshold:break
+    
+        D = calW(D,am,y,y_Gmx)
+        
+    return Gmx_all
     
 
 if __name__ == '__main__':
     x, y = loadData()
-    Gx = AdaBoost(x,y,m=5)
+    Gx = AdaBoost(x,y,maxIterNum = 100,errorThreshold = 0.01)
+    print(Gx)
     
 
     
